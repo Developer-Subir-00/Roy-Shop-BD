608d35f60959b79ea5932abd1fc7f2066a3a86e9
Developer-Subir-00 <developersr00@gmail.com>
2025-12-06 20:22:37 +0600

    code update

--- components/RelatedProducts.tsx
+++ components/RelatedProducts.tsx
@@ -9,6 +9,7 @@ import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
 import AddToCartButton from "./AddToCartButton";
 import FavoriteButton from "./FavoriteButton";
+import { image } from "@/sanity/image";
 
 interface RelatedProductsProps {
   currentProduct: Product;
@@ -54,10 +55,9 @@ const RelatedProducts = memo(({ relatedProducts }: RelatedProductsProps) => {
                       href={`/product/${product?.slug?.current}`}
                       className="block hover:text-shop_light_green transition-colors"
                     >
-                      <Image
+                      <img
                         src={imageUrl}
                         alt={"productImage"}
-                        fill
                         className="object-cover group-hover:scale-105 transition-transform duration-300"
                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
                       />
